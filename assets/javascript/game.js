4e6098b7c6b299188d7803166ead46b5af341b8e
CaoKao <bksaechao@gmail.com>
2021-02-02 05:32:10 -0800

    stuck on character select logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,73 @@
 var myRpg = {
     characters: [
-        characterOne = {
+        afroSamurai = {
+            id: "afroSamurai",
+            name: "Afro Samurai",
             image: "assets/images/afroSamurai.jpg",
-            power: 1,
+            power: 160,
             health: null
         },
-        characterTwo = {
+        dante = {
+            id: "dante",
+            name: "Dante",
             image: "assets/images/dante.png",
-            power: 2,
+            power: 200,
             health: null
         },
-        characterThree = {
+        samuraiJack = {
+            id: "samuraiJack",
+            name: "Samurai Jack",
             image: "assets/images/samuraiJack.png",
-            power: 3,
+            power: 150,
             health: null
         },
-        characterFour = {
+        vashTheStampede = {
+            id: "vash",
+            name: "Vash The Stampede",
             image: "assets/images/vash.png",
-            power: 4,
+            power: 180,
             health: null
         }
     ],
+
+    userHero: null,
 
     displayCharacters: function () {
         this.characters.forEach(character => {
             var charDiv = $("<div>");
             var charImg = $("<img>");
-            charImg.attr({"src":character.image, "alt":character});
-            charImg.css({ "height": "250", "width": "200" });
+            var charName = $("<h1>");
+            var charStats = $("<h2>");
+            $(charDiv).addClass("col-md-2 hero");
+            $(charName).text(character.name);
+            $(charStats).text(character.power);
+            charDiv.attr("id", character.id);
+            charDiv.css("position", "relative");
+            charName.css({ "position": "absolute", "top": 200, "color": "yellow", "text-align": "center" });
+            charStats.css({ "position": "absolute", "top": 0, "color": "red", "text-align": "center" });
+            charImg.attr({ "src": character.image, "alt": character.name });
+            charImg.css({ "height": "300", "width": "175" });
             charDiv.append(charImg);
+            charDiv.append(charName);
+            charDiv.append(charStats);
             $(".characters").append(charDiv);
-            console.log(charImg.alt);
+        })
+    },
+
+    handleCharSelect: function () {
+        this.characters.forEach(character => {
+            if (charId === character.id) {
+                this.userHero = character;
+                console.log(character);
+            }
         })
     }
 };
 
 $(document).ready(function () {
     myRpg.displayCharacters();
+    $(".hero").on("click", function () {
+        var charId = $(this).attr("id");
+        console.log(charId);
+    })
 })
