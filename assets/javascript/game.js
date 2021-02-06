5f277ad9445a981cb4ec3362a2918a2e5c31d838
CaoKao <bksaechao@gmail.com>
2021-02-05 23:27:45 -0800

    Fixed click event bug with event delegation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,49 +4,54 @@ var myRpg = {
             id: "afroSamurai",
             name: "Afro Samurai",
             image: "assets/images/afroSamurai.jpg",
-            power: 160,
-            health: null
+            health: 160,
+            power: 10,
+            isEnemy: false
         },
         dante = {
             id: "dante",
             name: "Dante",
             image: "assets/images/dante.png",
-            power: 200,
-            health: null
+            health: 200,
+            power: 10,
+            isEnemy: false
         },
         samuraiJack = {
             id: "samuraiJack",
             name: "Samurai Jack",
             image: "assets/images/samuraiJack.png",
-            power: 150,
-            health: null
+            health: 150,
+            power: 10,
+            isEnemy: false
         },
         vashTheStampede = {
             id: "vash",
             name: "Vash The Stampede",
             image: "assets/images/vash.png",
-            power: 180,
-            health: null
+            health: 180,
+            power: 10,
+            isEnemy: false
         }
     ],
 
     userHero: null,
+    isBattling: false,
 
     displayCharacters: function () {
         this.characters.forEach(character => {
             var charDiv = $("<div>");
             var charImg = $("<img>");
-            var charName = $("<h1>");
+            var charName = $("<h3>");
             var charStats = $("<h2>");
             $(charDiv).addClass("col-md-2 hero");
             $(charName).text(character.name);
-            $(charStats).text(character.power);
+            $(charStats).text(character.health);
             charDiv.attr("id", character.id);
             charDiv.css("position", "relative");
-            charName.css({ "position": "absolute", "top": 200, "color": "yellow", "text-align": "center" });
+            charName.css({ "position": "absolute", "top": 180, "color": "purple", "text-align": "center" });
             charStats.css({ "position": "absolute", "top": 0, "color": "red", "text-align": "center" });
             charImg.attr({ "src": character.image, "alt": character.name });
-            charImg.css({ "height": "300", "width": "175" });
+            charImg.css({ "height": "225", "width": "150" });
             charDiv.append(charImg);
             charDiv.append(charName);
             charDiv.append(charStats);
@@ -54,63 +59,87 @@ var myRpg = {
         })
     },
 
+    handleQuery: function(charId) {
+        this.handleCharSelect(charId);
+        this.handleBattle(charId);
+    },
+
     handleCharSelect: function (charId) {
-        $(".characters").empty();
-        $(".enemies").empty();
-        this.characters.forEach(character => {
-            if (charId == character.id) {
-                this.userHero = character;
-                console.log("====================Hero====================");
-                console.log(character);
-                console.log("============================================");
-                var charDiv = $("<div>");
-                var charImg = $("<img>");
-                var charName = $("<h1>");
-                var charStats = $("<h2>");
-                $(charDiv).addClass("col-md-3 hero");
-                $(charName).text(character.name);
-                $(charStats).text(character.power);
-                charDiv.attr("id", character.id);
-                charDiv.css("position", "relative");
-                charName.css({ "position": "absolute", "top": 200, "color": "yellow", "text-align": "center" });
-                charStats.css({ "position": "absolute", "top": 0, "color": "red", "text-align": "center" });
-                charImg.attr({ "src": character.image, "alt": character.name });
-                charImg.css({ "height": "300", "width": "175" });
-                charDiv.append(charImg);
-                charDiv.append(charName);
-                charDiv.append(charStats);
-                $(".characters").append(charDiv);
-                $(".character-name").text("");
-            } else {
-                console.log("====================Enemy===================");
-                console.log(character);
-                console.log("============================================");
-                var charDiv = $("<div>");
-                var charImg = $("<img>");
-                var charName = $("<h1>");
-                var charStats = $("<h2>");
-                $(charDiv).addClass("col-md-3 hero");
-                $(charName).text(character.name);
-                $(charStats).text(character.power);
-                charDiv.attr("id", character.id);
-                charDiv.css("position", "relative");
-                charName.css({ "position": "absolute", "top": 200, "color": "yellow", "text-align": "center" });
-                charStats.css({ "position": "absolute", "top": 0, "color": "red", "text-align": "center" });
-                charImg.attr({ "src": character.image, "alt": character.name });
-                charImg.css({ "height": "300", "width": "175" });
-                charDiv.append(charImg);
-                charDiv.append(charName);
-                charDiv.append(charStats);
-                $(".enemies").append(charDiv);
-            }
-        })
+        if (!this.isBattling){
+            $(".characters").empty();
+            // $(".enemies").empty();
+            this.characters.forEach(character => {
+                if (charId == character.id) {
+                    this.userHero = character;
+                    console.log("====================Hero====================");
+                    console.log(this.userHero);
+                    console.log("============================================");
+                    var charDiv = $("<div>");
+                    var charImg = $("<img>");
+                    var charName = $("<h3>");
+                    var charStats = $("<h2>");
+                    $(charDiv).addClass("col-md-3 hero");
+                    $(charName).text(character.name);
+                    $(charStats).text(character.health);
+                    charDiv.attr("id", character.id);
+                    charDiv.css("position", "relative");
+                    charName.css({ "position": "absolute", "top": 180, "color": "purple", "text-align": "center" });
+                    charStats.css({ "position": "absolute", "top": 0, "color": "red", "text-align": "center" });
+                    charImg.attr({ "src": character.image, "alt": character.name });
+                    charImg.css({ "height": "225", "width": "150" });
+                    charDiv.append(charImg);
+                    charDiv.append(charName);
+                    charDiv.append(charStats);
+                    $(".characters").append(charDiv);
+                    $(".character-name").text("Hero");
+                } else {
+                    character.isEnemy = true;
+                    console.log("====================Enemy===================");
+                    console.log(character);
+                    console.log("============================================");
+                    var charDiv = $("<div>");
+                    var charImg = $("<img>");
+                    var charName = $("<h3>");
+                    var charStats = $("<h2>");
+                    $(charDiv).addClass("col-md-3 hero");
+                    $(charName).text(character.name);
+                    $(charStats).text(character.health);
+                    charStats.attr("class", "char-stats")
+                    charDiv.attr("id", character.id);
+                    charDiv.css("position", "relative");
+                    charName.css({ "position": "absolute", "top": 180, "color": "purple", "text-align": "center" });
+                    charStats.css({ "position": "absolute", "top": 0, "color": "red", "text-align": "center" });
+                    charImg.attr({ "src": character.image, "alt": character.name });
+                    charImg.css({ "height": "225", "width": "150" });
+                    charDiv.append(charImg);
+                    charDiv.append(charName);
+                    charDiv.append(charStats);
+                    $(".enemies").append(charDiv);
+                    $(".enemies-name").text("Enemies");
+                }
+            });
+            this.isBattling = true;        
+        }
+    },
+
+    handleBattle: function (charId) {
+        if (this.isBattling) {
+            this.characters.forEach(character => {
+                if (charId === character.id && character.isEnemy) {
+                    // character.health -= this.userHero.power;
+                    console.log(character);
+                }
+            })
+        }
     }
 };
 
 $(document).ready(function () {
     myRpg.displayCharacters();
-    $(".hero").on("click", function () {
-        var charId = $(this).attr("id");
-        myRpg.handleCharSelect(charId);
+    var charId
+    $(document).on('click', ".hero", function() {
+        charId = $(this).attr("id");
+        console.log(charId);
+        myRpg.handleQuery(charId);
     })
-})
+});
